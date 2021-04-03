6029ac5e8dc40a53c5e9cee6243aefcc4433e5ba
Gabriel Silva Alves <gabrielsalves2k18@gmail.com>
2021-04-03 19:54:34 -0300

    db.json adicionado, ajuste no css

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export const api = axios.create({
+    baseURL: 'http://localhost:5000'
+})
+
+export const buscaApi = async(url, setDado) => {
+    const resposta = await api.get(url)
+    setDado(resposta.data);
+    console.log(resposta.data);
+}
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,20 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import '../assets/css/Card.css';
+import { buscaApi } from '../api/api';
 
 const Card = () => {
     const [jogosCard, setJogosCard] = useState([]);
 
     useEffect(() => {
-        setJogosCard([{
-            id: 1,
-            nome: 'Diablo 3',
-            img: 'https://upload.wikimedia.org/wikipedia/pt/1/12/DiabloIIIcover.jpg'
-        },{
-            id: 2,
-            nome: 'CS:GO',
-            img: 'https://notadogame.com/uploads/game/cover/250x/5bfdc36b0c6b7.jpg'
-        }]);
+        buscaApi(`/jogos`, setJogosCard)
     }, [])
 
     return (
